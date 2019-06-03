35846ea09cbfff3e7bf8c4fa5a74a3ee18a4a0a9
lmay <lmay@lmaye.com>
2019-06-03 15:34:43 +0800

    -- init project.

---
+++ spring-cloud-nacos/src/test/java/com/lmaye/spring/cloud/nacos/SpringCloudNacosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lmaye.spring.cloud.nacos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudNacosApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
