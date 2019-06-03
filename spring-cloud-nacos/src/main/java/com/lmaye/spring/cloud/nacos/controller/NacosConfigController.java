38fa987a20d56432158eb6d02388bb8924843dd5
lmay <lmay@lmaye.com>
2019-06-03 18:29:25 +0800

    -- init project.

---
+++ spring-cloud-nacos/src/main/java/com/lmaye/spring/cloud/nacos/controller/NacosConfigController.java
@@ -0,0 +1,12 @@
+package com.lmaye.spring.cloud.nacos.controller;
+
+/**
+ * -- Nacos配置Controller
+ *
+ * @author lmay.Zhou
+ * @date 2019/6/3 18:28 星期一
+ * @qq 379839355
+ * @email lmay@lmaye.com
+ */
+public class NacosConfigController {
+}
